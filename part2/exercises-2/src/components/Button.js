eb715804e0802d68690aac5c648d53a4c87cf0ad
Esther Onchieku <estheronchieku@gmail.com>
2023-08-07 16:02:16 -0500

    Banner.js and Button.js modified

--- part2/exercises-2/src/components/Button.js
+++ part2/exercises-2/src/components/Button.js
@@ -1,9 +1,12 @@
 import './styles.css';
 
 function Button() {
+   function onLearnMore(){
+      alert("splaaaaash");
+   }
 
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
